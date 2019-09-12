0683218b3c37aa916f582ce0896d316a7ab5410f
crowjambo <evaldascrow@gmail.com>
2019-09-12 16:38:16 +0300

    Task2 beginning

---
+++ Task2/Program.cs
@@ -0,0 +1,27 @@
+﻿// Parašykite programą, kuri apskaičiuoja studento pažymių vidurkį, atsižvelgdama į dalykų apimtį kreditais.
+
+// Pagrindiniai reikalavimai:
+
+//     Programa turi leisti įvesti studento duomenis (vardas, pavarde, grupė) ir išklausytus dalykus (pavadinimas, įvertinimas, kreditų kiekis) klaviatūra. (7 taškai)
+//     Programa ekrane turi išvesti pilną studento informaciją ir apskaičiuotojo vidurkio reikšmę. (8 taškai)
+
+// Papildomas reikalavimas:
+
+//     Programos architektūra turi atitikti vienafunkciškumo principą (angl. Single Responsibility). (10 taškų)
+
+//EVALDAS PAULAUSKAS PI18E
+//TASK 2
+
+
+using System;
+
+namespace Task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
