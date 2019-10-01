51272deca790eaafc7615a48e150734339e3ce71
crowjambo <evaldascrow@gmail.com>
2019-10-01 09:05:04 +0300

    planning completed

--- Task3/Program.cs
+++ Task3/Program.cs
@@ -1,4 +1,16 @@
-﻿using System;
+﻿
+//Programa turi leisti įvesti naują studentų grupę(pavadinimas, semestro numeris), grupei priskirti dėstomus dalykus(pavadinimas, kreditų kiekis) ir grupei priklausančius studentus. (10 taškų)
+//    Programa turi leisti įvesti pasirinkto studento pažymius ir priskirti juos atitinkamiems dalykams. (5 taškai)
+//    Programa turi apskaičiuoti ir išvesti studento bendrą pažymių vidurkį. (5 taškai)
+//    Programa turi apskaičiuoti ir išvesti visos grupės bendrą pažymių vidurkį. (5 taškai)
+//    Programa turi išvesti grupės, pagal didžiausią vidurkį, studentų Top 3. (5 taškai)
+
+
+
+//EVALDAS PAULAUSKAS PI18E
+
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
